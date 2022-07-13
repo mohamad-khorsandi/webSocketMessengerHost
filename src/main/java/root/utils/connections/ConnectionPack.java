692d56cfa4688394aafa5b09f048f2e5c784a626
mmd <9m.kh557@gmail.com>
2022-07-13 12:06:43 +0430

    auto formatter deleted

--- src/main/java/root/utils/connections/ConnectionPack.java
+++ src/main/java/root/utils/connections/ConnectionPack.java
@@ -5,13 +5,14 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Arrays;
+import java.util.Formatter;
 import java.util.Scanner;
 
 abstract public class ConnectionPack implements Closeable {
     public static NormalConnectionPack newNormConnectionPack(String ip, int port) throws IOException {
         NormalConnectionPack con = new NormalConnectionPack();
         con.socket = new Socket(ip, port);
-        con.send = new AutoFormatter(con.socket.getOutputStream());
+        con.send = new Formatter(con.socket.getOutputStream());
         con.receive = new Scanner(con.socket.getInputStream());
         return con;
     }
@@ -20,7 +21,7 @@ abstract public class ConnectionPack implements Closeable {
         NormalConnectionPack con = new NormalConnectionPack();
         ServerSocket serverSocket = new ServerSocket(port);
         con.socket = serverSocket.accept();
-        con.send = new AutoFormatter(con.socket.getOutputStream());
+        con.send = new Formatter(con.socket.getOutputStream());
         con.receive = new Scanner(con.socket.getInputStream());
         serverSocket.close();
         return con;
@@ -31,7 +32,7 @@ abstract public class ConnectionPack implements Closeable {
     public static MultiReceiveConnectionPack newMulRecConnectionPack(String ip, int port) throws IOException {
         MultiReceiveConnectionPack con = new MultiReceiveConnectionPack();
         con.socket = new Socket(ip, port);
-        con.send = new AutoFormatter(con.socket.getOutputStream());
+        con.send = new Formatter(con.socket.getOutputStream());
         con.receive = new MultiReceiveScanner(con.socket.getInputStream());
         return con;
     }
@@ -40,17 +41,18 @@ abstract public class ConnectionPack implements Closeable {
         MultiReceiveConnectionPack con = new MultiReceiveConnectionPack();
         ServerSocket serverSocket = new ServerSocket(port);
         con.socket = serverSocket.accept();
-        con.send = new AutoFormatter(con.socket.getOutputStream());
+        con.send = new Formatter(con.socket.getOutputStream());
         con.receive = new MultiReceiveScanner(con.socket.getInputStream());
         serverSocket.close();
         return con;
     }
 
     protected Socket socket;
-    protected AutoFormatter send;
+    protected Formatter send;
 
     public void format(String format, Object... args){
-        send.format(format, args);
+        send.format(format+"\n", args);
+        send.flush();
     }
 
     protected static void closeAll(Closeable... closeables) {
